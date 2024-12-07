b739c6bae639ebd3ab35a408ed2dce741a1b32e7
orest40th <149381887+orest40th@users.noreply.github.com>
2024-12-07 12:05:15 +0200

    Revert "configured spring boot, created dao,service,entity layers" (#3)

--- src/main/java/com/mate/academy/demo/exception/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.mate.academy.demo.exception;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
