57216af05e10fbeb2007718e927263fc500f5b82
Orest Sorokowey <osorokowey@gmail.com>
2025-02-20 20:56:46 +0200

    Revert "add User management, User DTOS, new user validator, exceptions"
    This reverts commit 47f347c8b6c06751dd396a6a8fa870f80c786a60.

--- src/main/java/com/mate/academy/demo/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.mate.academy.demo.service;
-
-import com.mate.academy.demo.dto.UserRegistrationRequestDto;
-import com.mate.academy.demo.dto.UserResponseDto;
-import com.mate.academy.demo.exception.RegistrationException;
-
-public interface UserService{
-    UserResponseDto save(UserRegistrationRequestDto requestDto) throws RegistrationException;
-}
