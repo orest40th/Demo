9cacae2101ff1a537dd6489be2d4728f89043228
Orest Sorokowey <osorokowey@gmail.com>
2025-03-26 11:32:18 +0200

    return validation annotations

--- src/main/java/com/mate/academy/demo/dto/CreateBookRequestDto.java
+++ src/main/java/com/mate/academy/demo/dto/CreateBookRequestDto.java
@@ -4,6 +4,7 @@ import com.mate.academy.demo.validation.Author;
 import com.mate.academy.demo.validation.CoverImage;
 import com.mate.academy.demo.validation.Description;
 import com.mate.academy.demo.validation.Title;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.util.Set;
@@ -36,5 +37,6 @@ public class CreateBookRequestDto {
     @CoverImage
     private String coverImage;
 
+    @NotBlank
     private Set<Long> categoryIds;
 }
